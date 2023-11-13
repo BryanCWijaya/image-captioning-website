d752cabb04c5c2e57ea54615674b2a88f89f5374
BryanCWijaya <82359659+BryanCWijaya@users.noreply.github.com>
2023-11-13 16:35:59 +0700

    Add new generation to left list.
    Also fix bugs:
    - Login and Signup background not full until bottom
    - Wrong title in Login and Signup page
    - Wrong heading in profile page

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ IMAGE_PATH = os.path.join(os.getcwd(), "images")
 @login_required
 def index():
     user_id = current_user.id
-    histories = PredictHistory.query.filter_by(user_id=user_id)
+    histories = PredictHistory.query.filter_by(user_id=user_id).order_by(PredictHistory.generated_date.desc())
     if "generation_id" in request.args:
         generation_id = request.args["generation_id"]
         pred_hist = PredictHistory.query.get(int(generation_id))
@@ -33,7 +33,7 @@ def load_image(filename):
 @login_required
 def profile():
     user_id = current_user.id
-    histories = PredictHistory.query.filter_by(user_id=user_id)
+    histories = PredictHistory.query.filter_by(user_id=user_id).order_by(PredictHistory.generated_date.desc())
     return render_template('profile.html', histories=histories)
 
 @main.route('/generate_caption', methods=['POST'])
--- app/transformers.py
+++ app/transformers.py
@@ -3,9 +3,9 @@ from PIL import Image
 
 captioners = {
     "vit-indobert": pipeline("image-to-text",model="models/vit-base-patch16-224 indobert-base-p1 batch_8 epoch_3"),
-    "dinov2-indobert": pipeline("image-to-text", model="models/dinov2-base indobert-base-p1 batch_8 epoch_3"),
-    "deit-indobert": pipeline("image-to-text", model="models/deit-base-distilled-patch16-224 indobert-base-p1 batch_8 epoch_3"),
-    "vit-gpt2": pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning"),
+    # "dinov2-indobert": pipeline("image-to-text", model="models/dinov2-base indobert-base-p1 batch_8 epoch_3"),
+    # "deit-indobert": pipeline("image-to-text", model="models/deit-base-distilled-patch16-224 indobert-base-p1 batch_8 epoch_3"),
+    # "vit-gpt2": pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning"),
 }
 
 def generate_caption(image, model):
